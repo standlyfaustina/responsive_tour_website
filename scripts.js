55cacc10154df934d7c7f6836c2a62a263f788ce
Mr Standly <standlyfaustina@gmail.com>
2021-06-10 20:52:19 +0100

    header

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 //start
+ 
+const selectElement = ({ s }) => document.querySelector(s);
+selectElement({ s: '.open' }).addEventListener('click', () => {
+    selectElement({ s: '.nav-list' }).classList.add('active');
+});
+selectElement({ s: '.close' }).addEventListener('click', () => {
+    selectElement({ s: '.nav-list' }).classList.remove('active');
+});  
+/* 
+const selectElement = (s) => document.querySelector(s);
+selectElement('.open').addEventListener('click', () => {
+    selectElement('nav-list').classList.add('active');
+});
+selectElement('.close').addEventListener('click', () => {
+    selectElement('nav-list').classList.remove('active');
+}); */
